6e2e502320697f78d6fe4c8350d45531fb9ad31e
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-03-02 17:35:12 +0700

    2 Add connect check

--- task2/main.c
+++ task2/main.c
@@ -1,45 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct _Matrix {
-  unsigned int size;
-  int **body;
-} Matrix;
+typedef struct _ListElem {
+  unsigned int value;
+  struct _ListElem *next;
+} ListElem;
 
-Matrix read_graph_from_file(char const *filename) {
+typedef struct _List { // Reference type
+  ListElem *head;
+} List;
+
+typedef struct _AdjacencyList {
+  unsigned int size;
+  List *body;
+} AdjacencyList;
+
+void push_front(List *list, unsigned int const value) {
+  ListElem *newElem = (ListElem*)malloc(sizeof(ListElem));
+  newElem->value = value;
+  newElem->next = list->head;
+  list->head = newElem;
+}
+
+AdjacencyList read_graph_from_file(char const *filename) {
   FILE *fin = fopen(filename, "r");
-  Matrix graph;
+  AdjacencyList graph;
   fscanf(fin, "%u\n", &graph.size);
-  graph.body = (int**)malloc(sizeof(int*) * graph.size);
-  for (unsigned int i = 0; i < graph.size; ++i) {
-    graph.body[i] = (int*)malloc(sizeof(int*) * graph.size);
-    for (unsigned j = 0; j < graph.size; ++j) {
-      if (i == j)
-        graph.body[i][j] = 1;
-      else
-        graph.body[i][j] = 0;
-    }
-  }
+  graph.body = (List*)malloc(sizeof(List) * graph.size);
   while (1) {
     unsigned int begin, end;
     if (fscanf(fin, "%u", &begin) == EOF)
       break;
     fscanf(fin, "%u", &end);
-    graph.body[begin][end] = 1;
+    push_front(graph.body + begin, end);
   }
   fclose(fin);
   return graph;
 }
 
-void mark_connected(Matrix const graph, unsigned int const vertex, char *visited) {
-  
+void mark_connected(AdjacencyList const graph, unsigned int const vertex, char *visited) {
+  visited[vertex] = 1;
+  for (ListElem *i = graph.body[vertex].head; i != NULL; i = i->next) {
+    visited[i->value] = 1;
+    mark_connected(graph, i->value, visited);
+  }
 }
 
-int is_all_connected(Matrix const graph) {
+int is_all_connected(AdjacencyList const graph) {
   char *visited = (char*)malloc(sizeof(char) * graph.size);
   for (unsigned int i = 0; i < graph.size; visited[i] = 0, ++i);
 
-  unsigend int const startVertex = 0;
+  unsigned int const startVertex = 0;
   mark_connected(graph, startVertex, visited);
 
   int isConnected = 1;
@@ -54,21 +65,29 @@ int is_all_connected(Matrix const graph) {
   return isConnected;
 }
 
-void delete_matrix(Matrix *matrix) {
-  for (unsigned int i = 0; i < matrix->size; ++i)
-    free(matrix->body[i]);
-  free(matrix->body);
+void delete_list(List *list) {
+  for (ListElem *i = list->head; i != NULL;) {
+    ListElem *next = i->next;
+    free(i);
+    i = next;
+  }
+}
+
+void delete_adjacency_list(AdjacencyList *graph) {
+  for (unsigned int i = 0; i < graph->size; ++i)
+    delete_list(graph->body + i);
+  free(graph->body);
 }
 
 int main() {
   char const *filename = "input.txt";
-  Matrix graph = read_graph_from_file(filename);
+  AdjacencyList graph = read_graph_from_file(filename);
 
   if (is_all_connected(graph))
     printf("Этот граф связный.\n");
   else
     printf("Этот граф несвязный.\n");
 
-  delete_matrix(&matrix);
+  delete_adjacency_list(&graph);
   return 0;
 }
