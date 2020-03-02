444d3bdec8c7164a97f8984b143ad271e2c6e540
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-03-02 12:38:41 +0700

    Fix mistakes in edge list conversion

--- task1/main.c
+++ task1/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 typedef struct _Edge {
-  unsigned int source, destination;
+  unsigned int begin, end;
   int weight;
   struct _Edge *next;
 } Edge;
@@ -25,13 +25,16 @@ EdgeList init_edge_list() {
 }
 
 void add_edge(EdgeList *graph, Edge const edge) {
-  Edge *tail;
-  for (tail = graph->head; tail != NULL; tail = tail->next);
-  tail = (Edge*)malloc(sizeof(Edge));
-  *tail = edge;
+  if (graph->head == NULL) { // Case of adding first elem
+    graph->head = (Edge*)malloc(sizeof(Edge));
+    *graph->head = edge;
+    return;
+  }
 
-  if (graph->head == NULL) // Case of adding first elem
-    graph->head = tail;
+  Edge *tail;
+  for (tail = graph->head; tail->next != NULL; tail = tail->next);
+  tail->next = (Edge*)malloc(sizeof(Edge));
+  *tail->next = edge;
 }
 
 EdgeList read_graph_from_file(char const *filename) {
@@ -40,10 +43,11 @@ EdgeList read_graph_from_file(char const *filename) {
   fscanf(fin, "%u\n", &size);
   EdgeList graph = init_edge_list();
   graph.size = size;
-  for (unsigned int i = 0; i < size; ++i) {
+  while (1) {
     Edge newEdge;
-    fscanf(fin, "%u", &(newEdge.source));
-    fscanf(fin, "%u", &(newEdge.destination));
+    if (fscanf(fin, "%u", &(newEdge.begin)) == EOF)
+      break;
+    fscanf(fin, "%u", &(newEdge.end));
     fscanf(fin, "%d", &(newEdge.weight));
     newEdge.next = NULL;
     add_edge(&graph, newEdge);
@@ -74,8 +78,8 @@ Matrix init_matrix_with_list(EdgeList const list) {
     }
   }
   for (Edge *tail = list.head; tail != NULL; tail = tail->next) {
-    matrix.body[tail->source][tail->destination] = tail->weight;
-    matrix.body[tail->destination][tail->source] = tail->weight;
+    matrix.body[tail->begin][tail->end] = tail->weight;
+    matrix.body[tail->end][tail->begin] = tail->weight;
   }
   return matrix;
 }
@@ -86,26 +90,26 @@ void delete_matrix(Matrix *matrix) {
   free(matrix->body);
 }
 
-int get_weight(Matrix const graph, unsigned int const source, unsigned int const destination) {
-  if (source > graph.size || destination > graph.size) {
+int get_weight(Matrix const graph, unsigned int const begin, unsigned int const end) {
+  if (begin > graph.size || end > graph.size) {
     printf("get_weight: Out of range error.\n");
     exit(1);
   }
-  return graph.body[source][destination];
+  return graph.body[begin][end];
 }
 
-unsigned int ask_source() {
+unsigned int ask_begin() {
   printf("What is edge begin? ");
-  unsigned int source;
-  scanf("%u", &source);
-  return source;
+  unsigned int begin;
+  scanf("%u", &begin);
+  return begin;
 }
 
-unsigned int ask_destination() {
+unsigned int ask_end() {
   printf("What is edge end? ");
-  unsigned int source;
-  scanf("%u", &source);
-  return source;
+  unsigned int begin;
+  scanf("%u", &begin);
+  return begin;
 }
 
 void print_weight(int const weight) {
@@ -121,9 +125,9 @@ int main() {
   EdgeList edgeList = read_graph_from_file(filename);
   Matrix matrix = init_matrix_with_list(edgeList);
   delete_edge_list(&edgeList);
-  unsigned int source = ask_source();
-  unsigned int destination = ask_destination();
-  int const weight = get_weight(matrix, source, destination);
+  unsigned int begin = ask_begin();
+  unsigned int end = ask_end();
+  int const weight = get_weight(matrix, begin, end);
   delete_matrix(&matrix);
   print_weight(weight);
   return 0;
