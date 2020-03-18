54b832f633e628de6ef1788eb8d7c12cc3cc3d6f
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-03-18 10:00:21 +0700

    4 Add draft of acyclic check

--- task4/src/main.c
+++ task4/src/main.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "AdjacencyList.h"
+#include "Queue.h"
+
+typedef enum _VertexStatus {NOT_PASSED, IN_QUEUE, PASSED} VertexStatus;
 
 AdjacencyList read_graph_from_file(char const *filename) {
   FILE *fin = fopen(filename, "r");
@@ -19,9 +22,40 @@ AdjacencyList read_graph_from_file(char const *filename) {
   return graph;
 }
 
+int is_acyclic(AdjacencyList const graph) {
+  VertexStatus *status = (VertexStatus*)malloc(sizeof(VertexStatus) * graph.size);
+  for (unsigned int vertex = 0; vertex < graph.size; ++vertex)
+    status[vertex] = NOT_PASSED;
+  Queue queue = make_queue();
+  unsigned int const startVertex = 0;
+  push(&queue, startVertex);
+  while (is_queue_empty(queue)) {
+    unsigned int const nextVertex = pop(&queue);
+    for (ListElem *i = graph.body[nextVertex].head; i != NULL; i = i->next) {
+      unsigned int const neighbourVertex = i->value;
+      if (status[neighbourVertex] == NOT_PASSED) {
+        push(&queue, neighbourVertex);
+        status[neighbourVertex] = IN_QUEUE;
+        continue;
+      }
+      if (status[neighbourVertex] == IN_QUEUE) {
+        delete_queue(&queue);
+        return 0;
+      }
+    }
+    status[nextVertex] = PASSED;
+  }
+  delete_queue(&queue);
+  return 1;
+}
+
 int main() {
   char const *inputFilename = "input.txt";
   AdjacencyList graph = read_graph_from_file(inputFilename);
+  if (is_acyclic(graph))
+    printf("YES\n");
+  else
+    printf("NO\n");
   delete_adjacency_list(&graph);
   return 0;
 }
