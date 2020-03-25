88999fd7663f2336f368660671d155f6ca96e2d0
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-03-25 17:50:21 +0700

    5 Add list module

---
+++ task5/include/List.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <stdlib.h>
+
+typedef struct _ListElem {
+  unsigned int value;
+  struct _ListElem *next;
+} ListElem;
+
+typedef struct _List {
+  ListElem *head;
+} List;
+
+List make_list();
+
+void push_front(List *list, unsigned int const value);
+
+unsigned int pop_back(List *list);
+
+void delete_list(List *list);
---
+++ task5/src/List.c
@@ -0,0 +1,43 @@
+#include <assert.h>
+#include "List.h"
+
+List make_list() {
+  List list;
+  list.head = NULL;
+  return list;
+}
+
+void push_front(List *list, unsigned int const value) {
+  ListElem *newElem = (ListElem*)malloc(sizeof(ListElem));
+  newElem->value = value;
+  newElem->next = list->head;
+  list->head = newElem;
+}
+
+unsigned int pop_back(List *list) {
+  assert(list->head != NULL);
+
+  // Deleting last element case
+  if (list->head->next == NULL) {
+    unsigned int const lastValue = list->head->value;
+    free(list->head);
+    list->head = NULL;
+    return lastValue;
+  }
+
+  // Deleting non-last element case
+  ListElem *preLast;
+  for (preLast = list->head; preLast->next->next != NULL; preLast = preLast->next);
+  unsigned int const lastValue = preLast->next->value;
+  free(preLast->next);
+  preLast->next = NULL;
+  return lastValue;
+}
+
+void delete_list(List *list) {
+  for (ListElem *i = list->head; i != NULL;) {
+    ListElem *next = i->next;
+    free(i);
+    i = next;
+  }
+}
