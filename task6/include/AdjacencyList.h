060e6dc318f95fee0a5c9e318ab26066b82994a0
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-04-12 13:27:42 +0700

    6 Update Adjacency list module to add ability checking and deleting edge

--- task6/include/AdjacencyList.h
+++ task6/include/AdjacencyList.h
@@ -12,4 +12,9 @@ AdjacencyList make_adj_list(unsigned int const size);
 
 void add_edge(AdjacencyList *graph, unsigned int const begin, unsigned int const end);
 
+int check_edge(AdjacencyList *graph, unsigned int const begin, unsigned int const end);
+
+// Doing nothing if edge does not exist
+void delete_edge(AdjacencyList *graph, unsigned int const begin, unsigned int const end);
+
 void delete_adjacency_list(AdjacencyList *graph);
--- task6/src/AdjacencyList.c
+++ task6/src/AdjacencyList.c
@@ -11,6 +11,26 @@ void add_edge(AdjacencyList *graph, unsigned int const begin, unsigned int const
   push_front(graph->body + begin, end);
 }
 
+int check_edge(AdjacencyList *graph, unsigned int const begin, unsigned int const end) {
+  List * neighbours = graph->body[begin];
+  for (ListElem * i = neighbours->head; i != NULL; i = i->next) {
+    if (i->value == end)
+      return 1;
+  }
+  return 0;
+}
+
+void delete_edge(AdjacencyList *graph, unsigned int const begin, unsigned int const end) {
+  List * neighbours = graph->body[begin];
+  for (ListElem * i = neighbours->head; i->next != NULL; i = i->next) {
+    if (i->next->value == end) {
+      ListElem *next = i->next->next;
+      free(i->next);
+      i->next = next;
+    }
+  }
+}
+
 void delete_adjacency_list(AdjacencyList *graph) {
   for (unsigned int i = 0; i < graph->size; ++i)
     delete_list(graph->body + i);
