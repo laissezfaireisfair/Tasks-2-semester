8830d2cce40c48ec1467a386ea3ad2659509bccb
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-05-04 19:40:59 +0700

    8 Add test for string Lists

---
+++ task8(lab7)/test/test1-List.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "String.h"
+#include "List.h"
+
+int main() {
+  List list = make_list();
+  if (list.head == NULL)
+    printf("Making list - OK\n");
+  else
+    printf("Making list - WRONG\n");
+
+  String str = make_str();
+  init_str(&str, 256);
+  assert(add_symbol('a', &str) == OK);
+  assert(add_symbol('b', &str) == OK);
+  assert(add_symbol('c', &str) == OK);
+  push_back(&list, str);
+  // Should be ["abc"]
+  int cmp1;
+  assert(compare_string(&str, &list.head->value, &cmp1) == OK);
+  if (cmp1 == 0)
+    printf("Push back   - OK\n");
+  else
+    printf("Push back   - WRONG\n");
+
+  String strFromList = pop_back(&list);
+  // Should be []
+  int cmp2;
+  assert(compare_string(&str, &list.head->value, &cmp2) == OK);
+  if (cmp2 != 0 && list.head == NULL)
+    printf("Pop back    - OK\n");
+  else
+    printf("Pop back    - WRONG\n");
+
+  delete_list(&list);
+  deinit_str(&str);
+  deinit_str(&strFromList);
+  return 0;
+}
