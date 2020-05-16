06b3633d77c24076f3d6bc522361c0bbe71d838d
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-05-16 13:43:59 +0700

    9 Add Queue module

---
+++ task9(lab8)/include/Queue.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdlib.h>
+#include "List.h"
+
+typedef struct _Queue {
+  List body;
+} Queue;
+
+Queue make_queue();
+
+void push_to_queue(Queue *queue, valType const value);
+
+valType pop_from_queue(Queue *queue);
+
+void delete_queue(Queue *queue);
+
+int is_queue_empty(Queue const queue);
