8a6509a670554c4a466235e3faea6f2a65aa4c1d
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-05-16 13:50:29 +0700

    9 Add empty main with all includes

---
+++ task9(lab8)/src/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Errors.h"
+#include "Constants.h"
+#include "Sequence.h"
+#include "Queue.h"
+#include "Matrix.h"
+
+
+int main() {
+  return 0;
+}
