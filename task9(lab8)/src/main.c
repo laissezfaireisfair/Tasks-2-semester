ff34275c61e02ad25ace6ceef78ff77acb70d2d0
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-05-16 15:08:36 +0700

    Add main draft without find_way() realisation

--- task9(lab8)/src/main.c
+++ task9(lab8)/src/main.c
@@ -6,14 +6,50 @@
 #include "Queue.h"
 #include "Matrix.h"
 
+error find_way(Matrix const graph, Sequence *way, unsigned int *distances) {}
 
 int main() {
+  Constants const constants = get_constants();
+
   Matrix graph;
   error const readStatus = read_graph_from_file(&graph);
   if (readStatus != OK) {
     print_error(readStatus);
     return 1;
   }
+
+  Sequence way = make_seq();
+  unsigned int *distances = NULL;
+  error const findStatus = find_way(graph, &way, distances);
+  if (findStatus != OK) {
+    print_error(findStatus);
+    delete_matrix(&graph);
+    return 2;
+  }
+
+  FILE *fout = fopen(constants.INPUT_FILENAME, "w");
+  if (fout == NULL) {
+    print_error(RUNTIME_ERROR);
+    delete_matrix(&graph);
+    return 3;
+  }
+
+  for (unsigned int i = 0; i < graph.size; ++i) {
+    if (fprintf(fout, "%u ", distances[i]) < 1) {
+      print_error(RUNTIME_ERROR);
+      delete_matrix(&graph);
+      return 4;
+    }
+  }
+  fprintf(fout, "\n");
+
+  error const printWayStatus = print_sequence(fout, way);
+  if (printWayStatus != OK) {
+    print_error(printWayStatus);
+    delete_matrix(&graph);
+    return 5;
+  }
+
   delete_matrix(&graph);
   return 0;
 }
