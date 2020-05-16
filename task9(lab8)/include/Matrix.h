1d1cb62082e6ff842ad795980e57f717a9b892f3
Andrey Golubev <laissezfaireisfair@protonmail.com>
2020-05-16 13:48:06 +0700

    9 Update Matrix module to have weight and to handle bad input

--- task9(lab8)/include/Matrix.h
+++ task9(lab8)/include/Matrix.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdlib.h>
 
 typedef struct _Matrix {
   unsigned int size;
@@ -9,9 +10,9 @@ Matrix init_matrix(unsigned int const size);
 
 void delete_matrix(Matrix *this);
 
-void add_edge(Matrix *this, unsigned int const begin, unsigned int const end);
+error add_edge(Matrix *this, unsigned int const begin, unsigned int const end);
 
-void del_edge(Matrix *this, unsigned int const begin, unsigned int const end);
+error del_edge(Matrix *this, unsigned int const begin, unsigned int const end);
 
 int check_edge(Matrix const *this, unsigned int const begin, unsigned int const end);
 
--- task9(lab8)/src/Matrix.c
+++ task9(lab8)/src/Matrix.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "Errors.h"
+#include "Constants.h"
 #include "Matrix.h"
 
 Matrix init_matrix(unsigned int const size) {
@@ -13,7 +14,7 @@ Matrix init_matrix(unsigned int const size) {
     if (this.body[i] == NULL)
       exit(RUNTIME_ERROR);
     for (unsigned int j = 0; j < size; ++j)
-      this.body[i][j] = 0;
+      this.body[i][j] = -1;
   }
   return this;
 }
@@ -24,18 +25,22 @@ void delete_matrix(Matrix *this) {
   free(this->body);
 }
 
-void add_edge(Matrix *this, unsigned int const begin, unsigned int const end) {
+error add_edge(Matrix *this, unsigned int const begin, unsigned int const end, int const weight) {
   if (begin >= this->size || end >= this->size)
-    exit(INVALID_ARGUMENT);
-  this->body[begin][end] += 1;
-  this->body[end][begin] += 1;
+    return INVALID_ARGUMENT;
+  if (this->body[begin][end] != -1 || this->body[end][begin] != -1)
+    return INVALID_ARGUMENT;
+  this->body[begin][end] = weight;
+  this->body[end][begin] = weight;
+  return OK;
 }
 
-void del_edge(Matrix *this, unsigned int const begin, unsigned int const end) {
+error del_edge(Matrix *this, unsigned int const begin, unsigned int const end) {
   if (begin >= this->size || end >= this->size)
-    exit(INVALID_ARGUMENT);
-  this->body[begin][end] -= 1;
-  this->body[end][begin] -= 1;
+    return INVALID_ARGUMENT;
+  this->body[begin][end] = -1;
+  this->body[end][begin] = -1;
+  return OK;
 }
 
 int check_edge(Matrix const *this, unsigned int const begin, unsigned int const end) {
@@ -44,43 +49,23 @@ int check_edge(Matrix const *this, unsigned int const begin, unsigned int const 
   return this->body[begin][end];
 }
 
-error count_graph_size(FILE * fin, unsigned int * size) {
-  *size = 0;
-  unsigned int numEdges;
-  fscanf(fin, "%u\n", &numEdges);
-
-  for (unsigned int i = 0; i < numEdges; ++i) {
-    unsigned int begin, end;
-
-    int const readBeginStatus = fscanf(fin, "%u", &begin);
-    if (readBeginStatus == EOF || readBeginStatus == 0)
-      return BAD_INPUT;
-    if (begin + 1 > *size)
-      *size = begin + 1;
-
-    int const readEndStatus = fscanf(fin, "%u", &end);
-    if (readEndStatus == EOF || readEndStatus == 0)
-      return BAD_INPUT;
-    if (end + 1 > *size)
-      *size = end + 1;
-  }
-
-  rewind(fin);
-  return OK;
-}
-
 error read_graph_from_file(char const *filename, Matrix * graph) {
+  Constants const constants = get_constants();
+
   FILE *fin = fopen(filename, "r");
-  unsigned int size;
-  error const statusCounting = count_graph_size(fin, &size);
-  if (statusCounting != OK)
-    return statusCounting;
-  *graph = init_matrix(size);
+  unsigned int numVertex;
+  fscanf(fin, "%u\n", &numVertex);
+  if (numVertex > constants.MAX_VERTEX)
+    return BAD_INPUT;
+  *graph = init_matrix(numVertex);
 
   unsigned int numEdges;
   fscanf(fin, "%u\n", &numEdges);
+  if (numEdges > constants.MAX_EDGES)
+    return BAD_INPUT;
+
   for (unsigned int i = 0; i < numEdges; ++i) {
-    unsigned int begin, end;
+    unsigned int begin, end, weight;
     int const readBeginStatus = fscanf(fin, "%u", &begin);
     if (readBeginStatus == EOF || readBeginStatus == 0) {
       delete_matrix(graph);
@@ -93,7 +78,22 @@ error read_graph_from_file(char const *filename, Matrix * graph) {
       fclose(fin);
       return BAD_INPUT;
     }
-    add_edge(graph, begin, end);
+    int const readWeightStatus = fscanf(fin, "%u", &weight);
+    if (readWeightStatus == EOF || readWeightStatus == 0) {
+      delete_matrix(graph);
+      fclose(fin);
+      return BAD_INPUT;
+    }
+
+    if (end >= numVertex || begin >= numVertex || weight > constants.MAX_INT)
+      return BAD_INPUT;
+
+    error const addStatus = add_edge(graph, begin, end, weight);
+    if (addStatus != OK) {
+      delete_matrix(graph);
+      fclose(fin);
+      return addStatus;
+    }
   }
 
   fclose(fin);
